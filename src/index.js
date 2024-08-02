c3c99736908fc6c24958f8d0a6c7608fa9cc9a62
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-08-02 19:27:33 +0300

    /

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import axios from 'axios';
 
+const selector = document.querySelector('.breed-select');
+axios.defaults.headers.common['x-api-key'] =
+  'live_vzM136oRXuJ5V9uRQMUR3QjMLKGpxlbuIV1e7lSsUS5RFEv9aGfSyshgBUm4saAO';
+
+let breeds = [];
+
+function fetchBreeds() {
+  axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(({ data }) =>
+      data.map(({ name, id }) => breeds.push({ text: name, value: id }))
+    );
+}
+new SlimSelect({
+  select: selector,
+  data: breeds,
+});
+console.log(breeds);
+fetchBreeds();
